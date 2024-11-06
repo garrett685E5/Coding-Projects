270e0a4e0fe14f8fc9d2ce4cf391744c8ad41dc0
garrett685E5 <tiffanimgarrett@gmail.com>
2024-11-06 13:18:24 -0500

    Add files via upload
    Add simple Calculator program

---
+++ Python/Garrett_Tiffani_Wk2_Program.py
@@ -0,0 +1,46 @@
+"""
+Name: Tiffani Garrett
+Problem: The purpose of this program is to create a calculator program that uses the following arithmetic operations (addition, subtraction, multiplication, and division).
+Date: 05/28/2024
+Version: 3.12
+"""
+
+#Display four arithmetic operations.
+
+
+#INPUT
+print ("Enter first number")
+number_A = int (input ("Enter first number"))
+print ("Enter second number")
+number_B = int (input ("Enter second number"))
+print ("Enter third number")
+number_C = int (input ("Enter third number"))
+print ("Enter fourth number")
+number_D = int (input("Enter fourth number"))
+
+#COMPUTE FOUR DIFFERENT OPERATIONS FROM THE USERS INPUT
+
+#ADDITION 
+sum= number_A + number_B
+print (number_A, "+" , number_B, "=", sum)
+
+#MULTIPLICATION
+product= number_A * number_B
+print (number_A, "*", number_B, "=" , product)
+
+#DIVISION
+outcome= number_C / number_D
+print (number_C, "/", number_D, "=", outcome)
+
+#SUBTRACTION
+result= number_C - number_D
+print (number_C, "-", number_D, "=", result)
+
+
+
+
+
+       
+
+
+ 
